ecf18e4a1023bfad0b09b5c69940a729b9888a40
Joshua A <82716852+helloworld22-joshua@users.noreply.github.com>
2023-10-13 11:10:18 +0200

    Update someFunctions.py

--- someFunctions.py
+++ someFunctions.py
@@ -1,6 +1,23 @@
-def reverseString(x):
-  return x[::-1]
+def myLambda(n):
+  return lambda a, b : (a + b) * n
 
+def printListReverse(my_numbers):
+  my_numbers.reverse()
+  [print(x) for x in my_numbers]
+
+def printListReverseAlternative(my_list):
+  for x in range(len(my_list) - 1, -1, -1):
+    print(my_list[x])
+
+def reverseString(my_string):
+  return my_string[::-1]
+
+addAndDouble = myLambda(2)
 myString = "Hello, World!"
+myNumbers = [1, 2, 3, 4, 5]
+myList = ["Mathe", "Englisch", "Deutsch"]
 
+print(addAndDouble(5, 10))
 print(reverseString(myString))
+printListReverse(myNumbers)
+printListReverseAlternative(myList)
