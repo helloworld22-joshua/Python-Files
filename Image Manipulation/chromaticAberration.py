3da5ec93ea278541a9d36323360da669e5410c5d
Joshua Albrecht <joshua.albrecht@mail.de>
2023-12-28 15:39:31 +0100

    New stuff

---
+++ Image Manipulation/chromaticAberration.py
@@ -0,0 +1,31 @@
+import time
+from PIL import Image
+
+img = Image.open(r'Image Manipulation\wild_anza.png')
+pix = img.load()
+
+new_img = Image.new(img.mode, img.size)
+new_pix = new_img.load()
+
+strength = 5                                                    # How far the values move from the original pixel
+
+for y in range(img.size[1]):
+
+    if not y % 100 or (y + 1) / img.size[1] == 1:
+        print(f"Progress: {round(y / img.size[1] * 100)}%")
+    
+    for x in range(img.size[0]):
+        r, g, b = pix[x, y]                                     # Current pixel
+
+        if x - strength >= 0:
+            _r, _g, _b = pix[x - strength, y]                   # New pixel on the left
+            new_pix[x - strength, y] = (r, _g, _b)              # Change value of red to the red value of current pixel
+
+        if x + strength < img.size[0]:
+            _r, _g, _b = pix[x + strength, y]                   # New pixel on the right
+            new_pix[x + strength, y] = (_r, _g, b)              # Change value of blue to the blue value of current pixel
+
+new_img.save(f"Image Manipulation\Results\chromaticAberration_{round(time.time() * 1000)}.png")
+
+img.close()
+new_img.close()
---
+++ Image Manipulation/invertColor.py
@@ -0,0 +1,29 @@
+import time
+from PIL import Image
+
+img = Image.open(r'Image Manipulation\lava_ship.png')
+pix = img.load()
+
+invertR = True
+invertG = False
+invertB = False
+
+for y in range(img.size[1]):
+
+    if not y % 100 or (y + 1) / img.size[1] == 1:
+        print(f"Progress: {round(y / img.size[1] * 100)}%")             # Status report every 100 pixels on the y-axis
+
+    for x in range(img.size[0]):
+        r, g, b = pix[x, y]
+
+        if invertR: r = 255 - r
+        if invertG: g = 255 - g
+        if invertB: b = 255 - b
+
+        pix[x, y] = (r, g, b)
+else:
+    print("Status: Completed!")
+
+img.save(f"Image Manipulation\Results\invertColor_{round(time.time() * 1000)}.png")
+
+img.close()
