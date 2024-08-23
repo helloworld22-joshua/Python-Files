709d06e7011d38f5503bcec06dfbe1db1709a780
Joshua Albrecht <joshua.albrecht@mail.de>
2024-08-23 12:43:08 +0200

    Added Gcd and lcm

--- Tutorial/smallestCommonMultiple.py
+++ Tutorial/smallestCommonMultiple.py
@@ -1,8 +1,51 @@
 import math
 
-a = 8
-b = 12
+a = 355
+b = 175
 
-lcm = abs(a * b) // math.gcd(a, b)
+gcd = math.gcd(a, b)
+lcm = abs(a * b) // gcd
+print(gcd, lcm)
 
-print(lcm)
+def nextPrime(prime):
+    while True:
+        prime += 1
+
+        for i in range(2, prime):
+            if prime % i == 0:
+                break
+        else:
+            return prime
+
+def primeFactorization(num):
+    pri = 2
+    exp = [0]
+
+    while num > 1:
+        if num % pri == 0:
+            num /= pri
+            exp[-1] += 1
+        else:
+            pri = nextPrime(pri)
+            exp.append(0)
+
+    return exp
+
+def calculateGcd(a, b):
+    a = primeFactorization(a)
+    b = primeFactorization(b)
+
+    pri = 2
+    gcd = 1
+
+    for i in range(min(len(a), len(b))):
+        gcd *= pri ** min(a[i], b[i])
+
+        pri = nextPrime(pri)
+
+    return gcd
+
+def calculateLcm(a, b):
+    return a * b // calculateGcd(a, b)
+
+print(calculateGcd(a, b), calculateLcm(a, b))
